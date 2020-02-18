f4e42e612ce8eacb73b700821a66d2c11978f654
Alex Gil <colibri.alex@gmail.com>
2020-02-17 20:15:24 -0500

    removed sx from PDF generation

--- utility/ConTeXt/convert.sh
+++ utility/ConTeXt/convert.sh
@@ -30,15 +30,13 @@ pandoc --template $root/utility/ConTeXt/template.unitTest -f markdown -t context
 echo Postprocess $filename
 
 
-
+ssed -r -i -f $root/utility/ConTeXt/titleItalics.ssed $filename.tex
 ssed -r -i -f $root/utility/ConTeXt/hyphenated.ssed $filename.tex
 ssed -r -i -f $root/utility/ConTeXt/thinrule.ssed $filename.tex
 ssed -r -i -f $root/utility/ConTeXt/iframe.ssed $filename.tex
 ssed -r -i -f $root/utility/ConTeXt/tables.ssed $filename.tex
-ssed -r -i -f $root/utility/ConTeXt/titleItalics.ssed $filename.tex
 ssed -r -i -f $root/utility/ConTeXt/epigraph.ssed $filename.tex
 ssed -r -i -f $root/utility/ConTeXt/captionFigure.ssed $filename.tex
-ssed -r -i -f $root/utility/ConTeXt/headerItalics.ssed $filename.tex
 echo ConTeXt $filename, log into $3 
 
 
